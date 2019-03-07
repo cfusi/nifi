69b03bd1ecda669d9a51e59e9011a1d5d2b4b8e8
cfusi <claudio.fusi@cloudera.com>
2019-03-07 17:08:46 +0100

    first commit

--- docker/minifi/start.sh
+++ docker/minifi/start.sh
@@ -4,8 +4,6 @@ tail -F "${MINIFI_HOME}/logs/minifi-app.log" &
 "${MINIFI_HOME}/bin/minifi.sh" run &
 minifi_pid="$!"
 
-# trap "echo Received trapped signal, beginning shutdown...;" KILL TERM HUP INT EXIT;
-
 while [ true ]
 do
    sleep 1000
